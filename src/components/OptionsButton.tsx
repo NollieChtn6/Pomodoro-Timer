1f212925fc010458d0e05151ed029bed3d1e960f
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-10-14 22:21:11 +0200

    create OptionsButton component

---
+++ src/components/OptionsButton.tsx
@@ -0,0 +1,17 @@
+import { Ellipsis } from "lucide-react";
+import { FastForward } from "lucide-react";
+
+type OptionsButtonProps = {
+  action: "openSettings" | "skipStep";
+  state: "isFocus" | "isShortBreak" | "isLongBreak";
+};
+
+export function OptionsButton({ action, state }: OptionsButtonProps) {
+  const icon =
+    action === "openSettings" ? <Ellipsis className="icon" /> : <FastForward className="icon" />;
+  return (
+    <button className={`btn options-btn ${state}`} type="button">
+      {icon}
+    </button>
+  );
+}
