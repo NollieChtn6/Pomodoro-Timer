c7dca486e2c4749754707610f6fc180721e4732c
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-10-15 00:12:58 +0200

    import ModalSettings into HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,25 +1,45 @@
+import { useState } from "react";
+
 import { Chip } from "../components/Chip";
 import { OptionsButton } from "../components/OptionsButton";
 import { StartStopButton } from "../components/StartStopButton";
 import { Timer } from "../components/Timer";
+import { SettingsModal } from "../components/ModalSettings";
 
 export function HomePage() {
+  const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
   const currentState = "isPaused";
   const timerIsRunning = false;
 
-  return (
-    <div className={`pomodoro-container ${currentState}`}>
-      <Chip state={currentState} />
+  const handleOpenModal = () => {
+    setIsModalVisible(true);
+  };
 
-      <Timer />
-      <div className="btns-container">
-        <OptionsButton action="openSettings" state={currentState} />
-        <StartStopButton state={currentState} timerIsRunning={timerIsRunning} />
-        <OptionsButton action="skipStep" state={currentState} />
+  const handleCloseModal = () => {
+    setIsModalVisible(false);
+  };
+
+  return (
+    <>
+      <div className={`pomodoro-container ${currentState}`}>
+        <Chip state={currentState} />
+        <Timer />
+        <div className="btns-container">
+          <OptionsButton action="openSettings" state={currentState} onClick={handleOpenModal} />
+          <StartStopButton state={currentState} timerIsRunning={timerIsRunning} />
+          <OptionsButton
+            action="skipStep"
+            state={currentState}
+            onClick={() => {
+              return null;
+            }}
+          />
+        </div>
+        <div className="step-container">
+          <p className="step">Next step is: Long Break (25min)</p>
+        </div>
       </div>
-      <div className="step-container">
-        <p className="step">Next step is: Long Break (25min)</p>
-      </div>
-    </div>
+      <SettingsModal isVisible={isModalVisible} onClose={handleCloseModal} />
+    </>
   );
 }
