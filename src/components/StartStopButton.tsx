97f1f2896f3abbeacf4af2e1688a8617b8765be7
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-10-14 22:21:51 +0200

    create StartStopButton component

---
+++ src/components/StartStopButton.tsx
@@ -0,0 +1,16 @@
+import { Play, Pause } from "lucide-react";
+
+type StartStopButtonProps = {
+  state: "isFocus" | "isShortBreak" | "isLongBreak";
+  timerIsRunning: boolean;
+};
+
+export function StartStopButton({ state, timerIsRunning }: StartStopButtonProps) {
+  const icon = timerIsRunning ? <Pause className="icon" /> : <Play className="icon" />;
+
+  return (
+    <button className={`btn start-stop-btn ${state}`} type="button">
+      {icon}
+    </button>
+  );
+}
