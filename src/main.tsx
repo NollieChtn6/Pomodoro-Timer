45564697e6ce6f17013443845f8c7f33fc90c804
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-10-14 22:19:20 +0200

    import reset.css into main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import "./reset.css";
 import "./index.css";
 
 // https://www.codingdeft.com/posts/react-18-typescript-error/
