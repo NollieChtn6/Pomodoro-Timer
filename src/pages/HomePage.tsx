65151d18a13838bf7bb871857bd7bc7198c822a3
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-10-14 22:54:45 +0200

    display next step

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,16 +4,21 @@ import { StartStopButton } from "../components/StartStopButton";
 import { Timer } from "../components/Timer";
 
 export function HomePage() {
-  const currentState = "isShortBreak";
+  const currentState = "isPaused";
   const timerIsRunning = false;
+
   return (
     <div className={`pomodoro-container ${currentState}`}>
       <Chip state={currentState} />
+
       <Timer />
       <div className="btns-container">
         <OptionsButton action="openSettings" state={currentState} />
         <StartStopButton state={currentState} timerIsRunning={timerIsRunning} />
         <OptionsButton action="skipStep" state={currentState} />
+      </div>
+      <div className="step-container">
+        <p className="step">Next step is: Long Break (25min)</p>
       </div>
     </div>
   );
